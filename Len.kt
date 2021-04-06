1a39ecb7dc502920ebd7f7b310be1a7a177c6cee
SirNapkin1334 <sirnapkin@protonmail.com>
2021-04-06 14:44:42 -0700

    Initial Commit

---
+++ Len.kt
@@ -0,0 +1,3 @@
+fun main() {
+	(""::toString::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke::invoke)()
+}
---
+++ Native.java
@@ -0,0 +1,3 @@
+public class Native {
+	public static native void main(final String[] args);
+}
